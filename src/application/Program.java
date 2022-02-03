e1ad688cbb712598f72f4b0ffd2816e6cdc958b1
João Fernando <jfmoraeslara@gmail.com>
2022-02-03 19:26:57 -0300

    Operação de conjuntos

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,35 +1,30 @@
 package application;
 
-import java.util.LinkedHashSet;
+import java.util.Arrays;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Set<String> set = new LinkedHashSet<>();
-
-		set.add("Tv");
-		set.add("Notebook");
-		set.add("Tablet");
+		//Operação por conjunto
+		Set<Integer> a = new TreeSet<>(Arrays.asList(0, 2, 4, 5, 6, 8, 10));
+		Set<Integer> b = new TreeSet<>(Arrays.asList(5, 6, 7, 8, 9, 10));
 		
-		// remove os elementos que atende a alguma característica
-		set.removeIf(x -> x.length() >= 3);
-		System.out.println("Removendo todos que tenham pelo menos três caracteres.");
+		// união
+		Set<Integer> c = new TreeSet<>(a);
+		c.addAll(b);
+		System.out.println(c);
 		
-		for (String p : set) {
-			System.out.println(p);
-		}
+		// intersecção
+		Set<Integer> d = new TreeSet<>(a);
+		d.retainAll(b);
+		System.out.println(d);
 		
-		System.out.println("------------------------------------------------------");
-		set.add("Tv");
-		set.add("Notebook");
-		set.add("Tablet");
-				
-		set.removeIf(x -> x.charAt(0) == 'T');
-		System.out.println("Removendo todos com o caractere inicial T.");
-		for (String p : set) {
-			System.out.println(p);
-		}
+		// diferença
+		Set<Integer> e = new TreeSet<>(a);
+		e.removeAll(b);
+		System.out.println(e);
 	}
 }
